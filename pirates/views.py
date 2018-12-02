5435cd7a15ac1d8fafbbb7a2386fd853d6d2c34f
Rodolfo <rodosilfer@hotmail.com>
2018-12-02 21:40:09 -0200

    commit

--- pirates/views.py
+++ pirates/views.py
@@ -4,23 +4,32 @@ from django.http import HttpResponseRedirect
 from django.views import View
 from django.forms import ModelForm
 from django.urls import reverse
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.list import  ListView
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin 
 
 from .models import Tesouro
 # Create your views here.
-class ListarTesouros(View):
-    def get(self,request):
-        lst_tesouros = Tesouro.objects.annotate(valor_total=ExpressionWrapper(F('quantidade')*F('preco'),\
+class ListarTesouros(LoginRequiredMixin,ListView):
+   
+     model = Tesouro
+     template_name = "lista_tesouros.html"
+     def get_queryset(self):
+        return Tesouro.objects.annotate(valor_total=ExpressionWrapper(F('quantidade')*F('preco'),\
                             output_field=DecimalField(max_digits=10,\
                                                     decimal_places=2,\
                                                      blank=True)\
                                                     )\
                             )
+def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
         valor_total = 0
-        for tesouro in lst_tesouros:
+        for tesouro in context.get('object_list',[]):
             valor_total += tesouro.valor_total
-        return render(request,"lista_tesouros.html",{"lista_tesouros":lst_tesouros,
-                                                     "total_geral":valor_total})
-class TesouroForm(ModelForm):
+        context.update({'total_geral': valor_total})
+        return context
+class TesouroForm(LoginRequiredMixin,ModelForm):
     class Meta:
         model = Tesouro
         fields = ['nome', 'quantidade', 'preco', 'img_tesouro']
@@ -28,25 +37,18 @@ class TesouroForm(ModelForm):
             "img_tesouro": "Imagem"
         }
 
-class SalvarTesouro(View):
-    def get_tesouro(self,id):
-        if id:
-            return Tesouro.objects.get(id=id)
-        return None
+class SalvarTesouro(LoginRequiredMixin,View):
+    model = Tesouro
+    fields = ['nome', 'quantidade', 'preco', 'img_tesouro']
+    template_name = "salvar_tesouro.html"
+    success_url = reverse_lazy('lista_tesouros')
 
-    def get(self,request,id=None):
-        return render(request,"salvar_tesouro.html",{"tesouroForm":TesouroForm(instance=self.get_tesouro(id))})
+class InserirTesouro(SalvarTesouro,CreateView):
+    pass
 
-    def post(self,request,id=None):
-        form = TesouroForm(request.POST,request.FILES, instance=self.get_tesouro(id))
+class AtualizarTesouro(SalvarTesouro,UpdateView):
+    pass
 
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect(reverse('lista_tesouros') )
-        else:
-            return render(request,"salvar_tesouro.html",{"tesouroForm":form})
-
-class RemoverTesouro(View):
-    def get(self,request,id):
-        Tesouro.objects.get(id=id).delete()
-        return HttpResponseRedirect(reverse('lista_tesouros') )
+class RemoverTesouro(DeleteView):
+    model = Tesouro
+    success_url = reverse_lazy('lista_tesouros')
